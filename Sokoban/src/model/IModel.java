c842a72bed760dfbe6d9756918db5cc6a6e39a4d
Maya Mill <mayam514@gmail.com>
2017-01-14 11:55:45 +0200

    adding the move methods and getLevel method

--- Sokoban/src/model/IModel.java
+++ Sokoban/src/model/IModel.java
@@ -1,5 +1,40 @@
 package model;
 
-public interface IModel {
+import model.data.Character;
+import model.data.Level;
 
+public interface IModel {
+	/**
+	 * The method calls the moveCharacterRight method from MoveItem class
+	 * @param _character the character that we want to move
+	 * @return if we moved the character 
+	 */
+	public boolean moveChararcterRight(Character character);
+	
+	/**
+	 * The method calls the moveCharacterLeft method from MoveItem class
+	 * @param character the character that we want to move
+	 * @return if we moved the character 
+	 */
+	public boolean moveChararcterLeft(Character character);
+	
+	/**
+	 * The method calls the moveCharacterUp method from MoveItem class
+	 * @param character the character that we want to move
+	 * @return if we moved the character 
+	 */
+	public boolean moveChararcterUp(Character character);
+	
+	/**
+	 * The method calls the moveCharacterDown method from MoveItem class
+	 * @param character the character that we want to move
+	 * @return if we moved the character 
+	 */
+	public boolean moveChararcterDown(Character character);
+	
+	/**
+	 * The method returns the current level
+	 * @return the current level
+	 */
+	public Level getLevel();
 }
