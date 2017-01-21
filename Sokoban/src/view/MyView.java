771b89247f3e706e9ffeef5d5e9f7eda1b4d3321
Maya Mill <mayam514@gmail.com>
2017-01-21 10:05:52 +0200

    add unimplemented methods

--- Sokoban/src/view/MyView.java
+++ Sokoban/src/view/MyView.java
@@ -2,6 +2,26 @@ package view;
 
 import java.util.Observable;
 
+import commons.Level;
+
 public class MyView extends Observable implements IView {
 
+	@Override
+	public void display(Level level) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void displayMessage(String msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
