27b3991a3e35e7eb08fbb59e923e758662b83f54
Maya Mill <mayam514@gmail.com>
2017-01-24 11:05:33 +0200

    change project to JavaFx

--- Sokoban/src/displayers/ILevelDisplayer.java
+++
@@ -1,7 +0,0 @@
-package displayers;
-
-import commons.Level;
-
-public interface ILevelDisplayer {
-	public void display(Level level);
-}
--- Sokoban/src/view/IView.java
+++
@@ -1,9 +0,0 @@
-package view;
-
-import commons.Level;
-
-public interface IView {
-	public void display(Level level);
-	public void displayMessage(String msg);
-	void start();
-}
